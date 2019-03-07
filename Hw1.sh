28f24df93d25e03557f43cf9ea25bde7e0a65c9e
Shero96 <48099843+Shero96@users.noreply.github.com>
2019-03-07 19:46:02 +0200

    Html-css_wo

---
+++ Hw1.sh
@@ -0,0 +1,22 @@
+mkdir ./abojan
+cd shero
+touch blank.txt
+
+for mm in 1 2 3 4 5 
+do
+
+echo -e'hello/n'>>greetings.txt
+done
+
+for mm in 1 2 3 4 5
+do
+cp greeting.txt $5.txt
+done 
+echo 'cat'>>pets.txt
+echo 'dog'>>pets.txt
+echo 'hamster'>>pets.txt
+
+echo 'cat'>>>commands.txt
+echo 'Ls'>>>commands.txt
+echo 'pwd'>>commands.txt
+cat pets .txt commands.txt | sort | uniq >> lovelycommands .txt
